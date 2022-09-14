266f853cbcdec2f4567b2b5e826edbe0cf63c2e0
HugoRodrigues <hugordsp@gmail.com>
2022-09-14 20:10:57 -0300

    Adicionei o arquivo extensions.py

--- __init__.py
+++ __init__.py
@@ -1,35 +1,16 @@
 import os
 os.system('cls')
-
 import os
 
-from flask import Flask
+from flask import Flask 
+from .extensions import db, migrate
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Bom dia!'
+    db.init_app(app)
+    migrate.init_app(app)
 
     return app
